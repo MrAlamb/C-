55bbc18b75bcaaccaddffe2186edbcf16236c7a9
MrAlamb <150518538+MrAlamb@users.noreply.github.com>
2023-11-11 13:45:31 +0300

    Add files via upload

---
+++ First/First/Fun.cs
@@ -0,0 +1,125 @@
+﻿using System;
+namespace First
+{
+    public class Fun
+    {
+        public Double funTan(double x) => Math.Tan(x);
+        public Double funCtan(double x) => Math.Cos(x)/Math.Sin(x);
+        public Double funSin(double x) => Math.Sin(x);
+        public Double funCos(double x) => Math.Cos(x);
+
+
+        public void Fun_tan(double DisplayX)
+        {
+
+            double epsilon = 0.073;
+            double y = 1.1;
+            double step = Math.PI / 25;        
+            while (y >= -1.1)
+            {
+                for (double x = (-DisplayX - 0.5) * Math.PI; x <= (DisplayX + 0.5) * Math.PI; x += step)
+                {
+                    if (Math.Abs(funTan(x) - y) >= epsilon)
+                        if (Math.Abs(y) < epsilon)
+                            Console.Write("-");
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+                y -= 0.1;
+            }
+        }
+
+
+
+        public void Fun_ctan(double DisplayX)
+        {
+
+            double epsilon = 0.073;
+            double y = 1.1;
+            double step = Math.PI / 25;    
+            while (y >= -1.1)
+            {
+                for (double x = -DisplayX * Math.PI; x <= DisplayX * Math.PI; x += step)
+                {
+                    if (Math.Abs(funCtan(x) - y) >= epsilon)
+                        if (Math.Abs(y) < epsilon)
+                            Console.Write("-");
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+                y -= 0.1;
+            }
+        }
+
+
+        public void Fun_cos(double DisplayX)
+        {
+
+            double epsilon = 0.04;
+            double y = 1.1;
+            double step = Math.PI / 30;       
+            while (y >= -1.1)
+            {
+                for (double x = -DisplayX * Math.PI; x <= DisplayX * Math.PI; x += step)
+                {
+                    if (Math.Abs(funCos(x) - y) >= epsilon)
+                        if (Math.Abs(y) < epsilon)
+                            Console.Write("-");
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+                y -= 0.1;
+            }
+        }
+
+
+        public void Fun_sin(double DisplayX)
+        {
+
+            double epsilon = 0.04;
+            double y = 1.1;
+            double step = Math.PI / 30;       
+            while (y >= -1.1)
+            {
+                for (double x = -DisplayX * Math.PI; x <= DisplayX * Math.PI; x += step)
+                {
+                    if (Math.Abs(funSin(x) - y) >= epsilon)
+                        if (Math.Abs(y) < epsilon)
+                            Console.Write("-");
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+                y -= 0.1;
+            }
+        }
+    }
+}
+
---
+++ First/First/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace First
+{
+    class MainClass
+    {
+        private const string Value = "Cin x:";
+
+        public static void Main(string[] args)
+        {
+          
+
+            void Graphic()
+            {
+                Console.Clear();
+                Console.WriteLine("Select graph type:");
+
+                int top = Console.CursorTop;
+                int y = top;
+
+                Console.WriteLine("1: sin");
+                Console.WriteLine("2: cos");
+                Console.WriteLine("3: tan");
+                Console.WriteLine("4: ctg");
+
+                int down = Console.CursorTop;
+
+                Console.CursorSize = 100;
+                Console.CursorTop = top;
+
+                ConsoleKey key;
+
+                while ((key = Console.ReadKey(true).Key) != ConsoleKey.Enter)
+                {
+                    switch (key)
+                    {
+                        case ConsoleKey.D1:
+                            y = top;
+                            Console.CursorTop = top;
+                            break;
+                        case ConsoleKey.D2:
+                            y = top + 1;
+                            Console.CursorTop = top + 1;
+                            break;
+                        case ConsoleKey.D3:
+                            y = top + 2;
+                            Console.CursorTop = top + 2;
+                            break;
+                        case ConsoleKey.D4:
+                            y = top + 3;
+                            Console.CursorTop = top + 3;
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+
+                Console.CursorTop = down;
+                Fun a = new Fun();
+                double b;
+                if (y == top)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Cin x:");
+                    a.Fun_sin(double.Parse(Console.ReadLine()));
+                }
+
+                else if (y == top + 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Cin x:");
+                    a.Fun_cos(double.Parse(Console.ReadLine()));
+                }
+                else if (y == top + 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Cin x:");
+                    a.Fun_tan(double.Parse(Console.ReadLine()));
+                }
+
+                else if (y == top + 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Cin x:");
+                    a.Fun_ctan(double.Parse(Console.ReadLine()));
+                }
+
+
+
+                //Console.ReadLine();
+
+            }
+
+            bool Flag = true;
+
+            while (Flag)
+
+            {
+                Graphic();
+                Console.WriteLine("1 - Again \n Any key - Close");
+                if (int.Parse(Console.ReadLine()) == 1) ;
+                else
+                {
+                    Console.WriteLine("Good luck");
+                    Flag = false;
+                }
+            }
+
+            Console.WriteLine("Good luck!");
+            Console.ReadLine();
+        }
+
+    }
+}
